160686a0eea7a99abf2372e3fd7be2ed7eb84139
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2022-12-01 15:10:48 +0000

    Forgotten file + day1 readme

--- 2022/utils/file_handler.py
+++ 2022/utils/file_handler.py
@@ -2,15 +2,16 @@ import os
 
 UTILS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-PROBLEM_INPUT_FORMATTER = "../day{day}/input_{problem}.txt"
+PUZZLE_INPUT_FORMATTER = "../day{day}/input_{part}.txt"
+PUZZLE_INPUT_FORMATTER_WITHOUT_PART = "../day{day}/input.txt"
 
-def get_problem_input(day: str, problem: str, debug=False) -> list[str]:
+def get_puzzle_input(day: str, part: int=-1, debug=False) -> list[str]:
     """
-    Basic function to get the problem input of a given day and problem (1 or 2).
+    Basic function to get the puzzle input of a given day and part (1 or 2).
     Also allows for a debug mode that provides detailed logging info.
     Returns the file contents as the list of line (strs).
     """
-    input_file = PROBLEM_INPUT_FORMATTER.format(day=day, problem=problem)
+    input_file = PUZZLE_INPUT_FORMATTER.format(day=day, part=part) if part > 0 else PUZZLE_INPUT_FORMATTER_WITHOUT_PART.format(day=day)
 
     if debug:
         print(f"Opening file {input_file}")
