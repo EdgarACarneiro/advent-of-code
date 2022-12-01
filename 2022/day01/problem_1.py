865721a20a40fa592bfeb64e54dc527ba05adc13
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2022-12-01 14:51:49 +0000

    Day 01 problem 1 finished

--- 2022/day01/problem_1.py
+++ 2022/day01/problem_1.py
@@ -1,7 +1,20 @@
 import utils.file_handler
 
 def main(input: list[str]) -> str:
-    pass
+    elfs_calories = [0]
+
+    elf_num = 0
+    for line in input:
+        # New elf parsing
+        if not len(line.strip()):
+            elf_num += 1
+            elfs_calories.append(0)
+            continue
+
+        # Otherwise, add calories to the current elf
+        elfs_calories[elf_num] += int(line.strip())
+
+    return max(elfs_calories)
 
 def test():
     test_input ="""
@@ -24,4 +37,4 @@ def test():
 
 if __name__ == "__main__":
     test()
-    print(main(utils.file_handler.get_problem_input("01", 1)))
+    print(main(utils.file_handler.get_problem_input("01", 1))) # 73211
