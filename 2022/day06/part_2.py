38b0c403f28593b3673389e8b5501bd891fe22d8
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2022-12-08 02:31:47 +0000

    Add day 6 part 2

---
+++ 2022/day06/part_2.py
@@ -0,0 +1,33 @@
+import utils.file_handler
+
+UNIQUE_CHARS_GOAL = 14
+
+
+def main(input: list[str]) -> int:
+    sw_begin = 0
+    sw_end = 1
+    input_str = input[0]
+
+    while sw_end + 1 <= len(input_str):
+        if input_str[sw_end] in input_str[sw_begin:sw_end]:
+            sw_begin += 1
+            continue
+        else:
+            sw_end += 1
+            if sw_end - sw_begin >= UNIQUE_CHARS_GOAL:
+                return sw_end
+
+    raise Exception(f"There is no start-of-packet marker in {input_str}")
+
+
+def test():
+    assert main("""mjqjpqmgbljsphdztnvjfqwrcgsmlb""".split("\n")) == 19
+    assert main("""bvwbjplbgvbhsrlpgdmjqwftvncz""".split("\n")) == 23
+    assert main("""nppdvjthqldpwncqszvftbrmjlhg""".split("\n")) == 23
+    assert main("""nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg""".split("\n")) == 29
+    assert main("""zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw""".split("\n")) == 26
+
+
+if __name__ == "__main__":
+    test()
+    print(main(utils.file_handler.get_puzzle_input("06")))
